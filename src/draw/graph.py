33f86a601fe6d0b011675507ff6440eaaa7aa1ad
Laurent Ngeth <l.ngeth@yahoo.fr>
2023-03-06 22:56:38 +0100

    replace to split

--- src/draw/graph.py
+++ src/draw/graph.py
@@ -5,22 +5,22 @@ import numpy as np
 class GraphSAT:
 
   def __init__(self, graph_s):
-    self.__graph_s = graph_s.replace(" ", "")
+    self.__graph_tab = [int(s) for s in graph_s.strip().split(" ")]
     self.string_to_graph()
 
   def string_to_graph(self):
-    s = self.__graph_s
-    if (len(s) < 2):
+    graph_tab = self.__graph_tab
+    print(graph_tab)
+    if (len(graph_tab) < 2):
       raise Exception("Le format du graphe donné est incorrect...")
-    if ((int(s[1]) * 2) != (len(s) - 2)):
+    elif ((graph_tab[1] * 2) != (len(graph_tab) - 2)):
       raise Exception("Des arètes n'ont pas été définies...")
     
     # graph is correct: add to Graph properties
-    self.__nb_node = int(s[0])
-    self.__nb_edge = int(s[1])
+    self.__nb_node = graph_tab[0]
     list_edge = []
-    for i in range (2, len(s)-1):
-      list_edge.append((int(s[i]), int(s[i+1])))
+    for i in range(2, len(graph_tab)-1, 2):
+      list_edge.append((graph_tab[i], graph_tab[i+1]))
     self.__list_edge = list_edge
 
   def draw_graph_sat(self):
