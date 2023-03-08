1ebbf1ecc2afaeb28056dfc8219cedcc7bbbf201
Laurent Ngeth <l.ngeth@yahoo.fr>
2023-03-08 21:20:10 +0100

    get result from glucose

--- src/draw/graph.py
+++ src/draw/graph.py
@@ -3,14 +3,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class GraphSAT:
+  """Represent a Graph in code
+
+  Attributes:
+    __graph_tab: List of each integer entered by the user (i.e. 2 1 1 2 => [2,1,1,2])
+    __nb_node: Number of nodes of the graph
+    __nb_edge: Number of edges of the graph
+    __list_edge: List of edges of the graph. List of tuples (i.e. [(1,2), (2,3), ...])
+  """
 
   def __init__(self, graph_s):
+    """Init the Graph object with String format Graph given by user"""
     self.__graph_tab = [int(s) for s in graph_s.strip().split(" ")]
     self.string_to_graph()
 
   def string_to_graph(self):
+    """Convert the string graph given by the user into Graph object"""
     graph_tab = self.__graph_tab
-    print(graph_tab)
     if (len(graph_tab) < 2):
       raise Exception("Le format du graphe donné est incorrect...")
     elif ((graph_tab[1] * 2) != (len(graph_tab) - 2)):
@@ -18,12 +27,49 @@ class GraphSAT:
     
     # graph is correct: add to Graph properties
     self.__nb_node = graph_tab[0]
+    self.__nb_edge = graph_tab[1]
     list_edge = []
     for i in range(2, len(graph_tab)-1, 2):
       list_edge.append((graph_tab[i], graph_tab[i+1]))
     self.__list_edge = list_edge
+  
+  def get_dimacs_clauses(self):
+    """Convert the Graph object into Dimacs format"""
+    nb_node = self.__nb_node
+    list_variable = np.arange(1, (nb_node*3)+1)
+
+    list_clause = []
+    list_clause.append("2 0") # first node is RED
+    
+    for i in range(0, len(list_variable), 3):
+      # at least a color
+      s = "{0} {1} {2} 0".format(list_variable[i], list_variable[i+1], list_variable[i+2])
+      list_clause.append(s)
+
+      # at most one color
+      s = "-{0} -{1} 0".format(list_variable[i], list_variable[i+1])
+      list_clause.append(s)
+      s = "-{0} -{1} 0".format(list_variable[i], list_variable[i+2])
+      list_clause.append(s)
+      s = "-{0} -{1} 0".format(list_variable[i+1], list_variable[i+2])
+      list_clause.append(s)
+    
+    # not same color of neighbor
+    for edge in self.__list_edge:
+      i = (edge[0]-1)*3
+      j = (edge[1]-1)*3
+      s = "-{0} -{1} 0".format(list_variable[i], list_variable[j])
+      list_clause.append(s)
+      s = "-{0} -{1} 0".format(list_variable[i+1], list_variable[j+1])
+      list_clause.append(s)
+      s = "-{0} -{1} 0".format(list_variable[i+2], list_variable[j+2])
+      list_clause.append(s)
+
+    head = "p cnf {} {}".format(nb_node*3, len(list_clause))
+    return (head, list_clause)
 
   def draw_graph_sat(self):
+    """Draw the Graph with Networkx"""
     G = nx.Graph()
     G.add_nodes_from(np.arange(1, self.__nb_node + 1))
     G.add_edges_from(self.__list_edge)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 # Import libraries
 import numpy as np
 import networkx as nx
+import re
 
 # Import tools class
 from interface.logs import *
 from draw.graph import *
+
+import subprocess # to run bash command
 
 startProgram()
 
@@ -12,7 +15,7 @@ line = ""
 while(True):
     # get line
     line = input()
-    print("Graph :", line)
+    print("Graph : {}\n".format(line))
     if (line == "exit"):
         break
     
@@ -21,7 +24,33 @@ while(True):
       g = GraphSAT(line)
 
       # draw Graph
-      g.draw_graph_sat()
+      print("To Dimacs format :")
+
+      # write Dimacs format into txt files
+      head, clauses = g.get_dimacs_clauses()
+      f = open("data/SAT_in.txt", mode="w")
+      f.write(head + "\n")
+      f.close()
+      f = open("data/SAT_in.txt", mode="a")
+      for c in clauses:
+        f.write(c + "\n")
+      f.close()
+      
+      f = open("data/SAT_in.txt", mode="r")
+      print(f.read())
+      f.close()
+
+      # run glucose
+      bash_command = "glucose -model data/SAT_in.txt"
+      process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+      output, error = process.communicate()
+      output = output.decode("utf-8")
+      # extract the result only
+      res = re.search(r's\s[A-Z]{0,3}SATISFIABLE', output)
+      output = output[res.start():]
+      print(output.strip())
+
+      # g.draw_graph_sat()
     except Exception as e:
        print("Erreur :", e)
        print("(Format voulu : nb_noeuds nb_aretes n1 n2 ...)")
